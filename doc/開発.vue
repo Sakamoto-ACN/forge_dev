7420c31ec7b6c8e2d3212565d4b194347d14dad0
takuya.sakamoto <takuya45004500@gmail.com>
2023-06-13 02:22:21 +0900

    画像生成

---
+++ doc/開発.vue
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>image sample</title>
+  <script src=https://unpkg.com/vue@next></script>
+ </head>
+
+ <body>
+  <button onclick="doBtn()">画像を表示</button>
+   <div id="app">
+    <img v-bind:src="imagename"></img>
+   </div>
+
+   <script>
+    new Vue({
+        el:'#app',
+        date:{
+            imagename:'sample.jpg'
+        }   
+    })
+    </script>
+  </body>
+ </html>   
